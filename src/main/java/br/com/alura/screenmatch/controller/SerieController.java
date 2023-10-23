5ed7bf3974e72138cc55295d1ce9a7bffac3618e
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2023-10-23 11:56:09 -0300

    Vídeo 2.1

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,13 +1,21 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repositorio;
+
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Aqui vão ser listadas as séries";
+    public List<Serie> obterSeries() {
+        return repositorio.findAll();
     }
 }
