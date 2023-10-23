e2a189b34ed749adc729d7502764d07e3569300d
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-10-23 11:16:12 -0300

    vídeo 1.3

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -8,17 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-	@Autowired
-	private SerieRepository repositorio;
+public class ScreenmatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
-	}
 }
