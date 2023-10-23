6c18435bf3df737ac8ac4cab28a4f88c1b4ff895
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-10-23 11:33:28 -0300

    vídeo 1.4

---
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SerieController {
+
+    @GetMapping("/series")
+    public String obterSeries() {
+        return "Aqui vão ser listadas as séries";
+    }
+}
